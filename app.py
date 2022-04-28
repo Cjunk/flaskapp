0d08e727f27f675084a789a19fcaf1a99bf3816b
Jericho Sharman <jsharman@hotmail.com.au>
2022-04-28 21:53:30 +1000

    secret_key

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import os
+
+DATABASE_URL = os.environ.get('DATABASE_URL','dbname=project2db') # dbname is the name of the local database
+SECRET_KEY = os.environ.get('SECRET_KEY','pretend secret key')
 
 app = Flask(__name__)
+app.config['SECRET_KEY']= SECRET_KEY
 
 @app.route('/')
 def index():
