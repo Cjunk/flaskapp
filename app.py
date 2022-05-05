356bbf1feea0fd6dabb2f5519547795ee5afe3d2
Jericho Sharman <jsharman@hotmail.com.au>
2022-05-05 21:24:11 +1000

    working registration

--- app.py
+++ app.py
@@ -65,25 +65,27 @@ def index():
     currentpricelist = getallprices()       # update from coinpot the latest prices. passed to the html
     if request.method == 'POST':    #   Path taken when registering
         print("In the POST METHOD path = '/' ")
-        registernewuser(request.form['nick_name'],request.form['fname'],request.form['lname'],request.form['pword'])
-        session['userid'] = loginuser(request.form['nick_name'],request.form['pword'])
+        session['userid'] = registernewuser(request.form['nick_name'],request.form['fname'],request.form['lname'],request.form['pword'])
+        session['portfolioID']= get_portfolioID_fromuserID(session['userid']) # ASSUMPTION of only one portfolio per person TODO Change this to accomodate multiple portfolios
+        session['nick_name'] = request.form['nick_name']
+        session['portfolios'] = getportfolio(session['userid']) #Add the porfolio summary to the session
+        session['showportfolio'] = 1 # variable to decide if we are displaying a portfolio or not
+    print("In the GET METHOD path = '/' ")
+    if session.get("userid") :
+        portfolioDetail = getportfolioDetail(session['portfolioID'])
+        session['portfolios'] = getportfolio(session['portfolioID']) #Add the porfolio summary to the session
+        response = []
+        totalcosts = []
+        if(portfolioDetail):
+            for each in portfolioDetail:
+                response.append(float(cryptoApi.latestPrice(each[2])) * float(each[PORTFOLIO_DETAIL_QTY]))
+                totalcosts.append(each[PORTFOLIO_DETAIL_PRICE] * each[PORTFOLIO_DETAIL_QTY])
+            totalcosts.append(sum(totalcosts))
+            response.append(sum(response))
+        return render_template(homepage,portfoliodetail=portfolioDetail,response=response,totalcosts=totalcosts,currentpricelist=currentpricelist)
     else:
-        print("In the GET METHOD path = '/' ")
-        if session.get("userid") :
-            portfolioDetail = getportfolioDetail(session['portfolioID'])
-            session['portfolios'] = getportfolio(session['portfolioID']) #Add the porfolio summary to the session
-            response = []
-            totalcosts = []
-            if(portfolioDetail):
-                for each in portfolioDetail:
-                    response.append(float(cryptoApi.latestPrice(each[2])) * float(each[PORTFOLIO_DETAIL_QTY]))
-                    totalcosts.append(each[PORTFOLIO_DETAIL_PRICE] * each[PORTFOLIO_DETAIL_QTY])
-                totalcosts.append(sum(totalcosts))
-                response.append(sum(response))
-            return render_template(homepage,portfoliodetail=portfolioDetail,response=response,totalcosts=totalcosts,currentpricelist=currentpricelist)
-        else:
-            return render_template(homepage,portfoliodetail="None")
-    return render_template(homepage,portfoliodetail=getportfolioDetail(1),currentpricelist=currentpricelist)
+        return render_template(homepage,portfoliodetail="None",currentpricelist=currentpricelist)
+    #return render_template(homepage,portfoliodetail=getportfolioDetail(1),currentpricelist=currentpricelist)
 
 @app.route('/register', methods=['GET','POST'])
 def register():
